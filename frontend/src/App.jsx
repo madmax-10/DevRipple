a09df554c4a40b57dac8cefb34744f890b5166cd
madmax-10 <ashbhatta234@gmail.com>
2025-06-15 15:54:42 -0500

    Add EditorPage3 component and update routing in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import LandingPage from './components/LandingPage'
 import EditorPage from './components/EditorPage'
 import './App.css'
 import EditorPage2 from './components/EditorPage2'
+import EditorPage3 from './components/EditorPage3'
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          <Route path="/editor" element={<EditorPage2 />} />
+          <Route path="/editor" element={<EditorPage />} />
         </Routes>
       </div>
     </Router>
